5421fe2553989013e55cb6b1a7a15541c06a548d
lgarstka1 <lgarstka@student.wsszib.edu.pl>
2026-03-25 22:22:32 +0100

    Poprawki3

--- app/seed/run_seed.py
+++ app/seed/run_seed.py
@@ -6,13 +6,11 @@ from src.app import app, db, Task
 def run_seed():
     print("Rozpoczynam seedowanie bazy danych...")
     
-    
     time.sleep(5)
    
     with app.app_context():
-           db.create_all()
+        db.create_all()
 
-        
         if Task.query.count() == 0:
             zadania = [
                 Task(name="Task1"),
@@ -30,12 +28,10 @@ def run_seed():
         output_dir = "/app/seed_output"
         os.makedirs(output_dir, exist_ok=True)
         
-        
         with open(f"{output_dir}/seed.log", "w", encoding="utf-8") as log_file:
             log_file.write("Seedowanie zakonczone sukcesem.\n")
             log_file.write("Dodano 5 zadan do tabeli Task.\n")
             
-     
         with open(f"{output_dir}/tasks.csv", "w", encoding="utf-8") as csv_file:
             csv_file.write("id,name\n")
  
